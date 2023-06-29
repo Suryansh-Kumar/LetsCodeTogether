b7c95675b83f8b0c342e3b8f0ad6478e94b4c105
Suryansh-Kumar <suryansh7250@gmail.com>
2023-06-29 10:08:49 +0530

    Initial commit with basic frontend

---
+++ script.js
@@ -0,0 +1,134 @@
+html = document.getElementById("html");
+python = document.getElementById("python");
+css = document.getElementById("css");
+js = document.getElementById("js");
+c = document.getElementById("c");
+tailwind = document.getElementById("tailwind");
+bootstrap = document.getElementById("bootstrap");
+main_con = document.getElementById("main-con");
+card_container_html = document.getElementById("card-container-html");
+card_container_python = document.getElementById("card-container-python");
+card_container_css = document.getElementById("card-container-css");
+card_container_js = document.getElementById("card-container-js");
+card_container_c = document.getElementById("card-container-c");
+card_container_tailwind = document.getElementById("card-container-tailwind");
+card_container_bootstrap = document.getElementById("card-container-bootstrap");
+
+function toggleHiddenHtml() {
+	if (card_container_html.hasAttribute("hidden")) {
+		card_container_html.removeAttribute("hidden");
+	} else {
+		card_container_html.setAttribute("hidden", "");
+	}
+}
+
+function toggleHiddenPython() {
+	if (card_container_python.hasAttribute("hidden")) {
+		card_container_python.removeAttribute("hidden");
+	} else {
+		card_container_python.setAttribute("hidden", "");
+	}
+}
+
+function toggleHiddenCSS() {
+	if (card_container_css.hasAttribute("hidden")) {
+		card_container_css.removeAttribute("hidden");
+	} else {
+		card_container_css.setAttribute("hidden", "");
+	}
+}
+
+function toggleHiddenJS() {
+	if (card_container_js.hasAttribute("hidden")) {
+		card_container_js.removeAttribute("hidden");
+	} else {
+		card_container_js.setAttribute("hidden", "");
+	}
+}
+
+function toggleHiddenC() {
+	if (card_container_c.hasAttribute("hidden")) {
+		card_container_c.removeAttribute("hidden");
+	} else {
+		card_container_c.setAttribute("hidden", "");
+	}
+}
+
+function toggleHiddenTailwind() {
+	if (card_container_tailwind.hasAttribute("hidden")) {
+		card_container_tailwind.removeAttribute("hidden");
+	} else {
+		card_container_tailwind.setAttribute("hidden", "");
+	}
+}
+
+function toggleHiddenBootstrap() {
+	if (card_container_bootstrap.hasAttribute("hidden")) {
+		card_container_bootstrap.removeAttribute("hidden");
+	} else {
+		card_container_bootstrap.setAttribute("hidden", "");
+	}
+}
+html.addEventListener("click", () => {
+	if (main_con.classList.contains("highlight")) {
+		main_con.classList.remove("highlight");
+	} else {
+		main_con.classList.add("highlight");
+	}
+	// main_con.classList.add("highlight");
+	toggleHiddenHtml();
+});
+python.addEventListener("click", () => {
+	if (main_con.classList.contains("highlight")) {
+		main_con.classList.remove("highlight");
+	} else {
+		main_con.classList.add("highlight");
+	}
+	// main_con.classList.add("highlight");
+	toggleHiddenPython();
+});
+css.addEventListener("click", () => {
+	if (main_con.classList.contains("highlight")) {
+		main_con.classList.remove("highlight");
+	} else {
+		main_con.classList.add("highlight");
+	}
+	// main_con.classList.add("highlight");
+	toggleHiddenCSS();
+});
+js.addEventListener("click", () => {
+	if (main_con.classList.contains("highlight")) {
+		main_con.classList.remove("highlight");
+	} else {
+		main_con.classList.add("highlight");
+	}
+	// main_con.classList.add("highlight");
+	toggleHiddenJS();
+});
+c.addEventListener("click", () => {
+	if (main_con.classList.contains("highlight")) {
+		main_con.classList.remove("highlight");
+	} else {
+		main_con.classList.add("highlight");
+	}
+	// main_con.classList.add("highlight");
+	toggleHiddenC();
+});
+tailwind.addEventListener("click", () => {
+	if (main_con.classList.contains("highlight")) {
+		main_con.classList.remove("highlight");
+	} else {
+		main_con.classList.add("highlight");
+	}
+	// main_con.classList.add("highlight");
+	toggleHiddenTailwind();
+});
+bootstrap.addEventListener("click", () => {
+	if (main_con.classList.contains("highlight")) {
+		main_con.classList.remove("highlight");
+	} else {
+		main_con.classList.add("highlight");
+	}
+	// main_con.classList.add("highlight");
+	toggleHiddenBootstrap();
+});
